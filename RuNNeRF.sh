8f47422173e1746ef80bcb274872e0a78bb3e959
ToolSeRF <codxragnar@gmail.com>
2024-08-09 01:25:07 +0300

    Update RuNNeRF.sh
    Add Backup Name

--- RuNNeRF.sh
+++ RuNNeRF.sh
@@ -6,6 +6,7 @@ read -p "FTP_PASSWORD: " FTP_PASSWORD
 read -p "BackupFile: " BackupFile
 read -p "HostDirectory: " HostDirectory
 read -p "Back UP in HOURs: " BackUPPERHOUR
+read -p "Backup File Prefix Name: " PRFNM
 cat <<EOL > backUPeRF.sh
 #!/bin/bash
 FTP_HOST="$FTP_HOST"
@@ -14,7 +15,7 @@ FTP_PASSWORD="$FTP_PASSWORD"
 LOCAL_FILE="$BackupFile"
 REMOTE_DIR="$HostDirectory"
 TIMESTAMP=\$(date +"%Y%m%d_%H%M%S")
-BACKUP_FILE="x-ui_\$TIMESTAMP.db"
+BACKUP_FILE="$PRFNM\$TIMESTAMP.db"
 lftp -u "\$FTP_USER","\$FTP_PASSWORD" "\$FTP_HOST" <<EOF
 set ftp:ssl-allow no
 put "\$LOCAL_FILE" -o "\$REMOTE_DIR/\$BACKUP_FILE"
